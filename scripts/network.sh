f6cec04361e57d72f43535a840f460ab94d6480d
kennethfan <kennethfan137@gmail.com>
2024-09-05 12:56:23 +0800

    network script fix

--- scripts/network.sh
+++ scripts/network.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-network=$(docker network ls | grep network | grep -v grep | wc -l)
+network=$(docker network ls | grep extnetwork | grep -v grep | wc -l)
 if [ $network -eq 0 ]; then
 	echo "docker network create -d bridge --subnet=172.16.0.0/16 extnetwork"
 	docker network create -d bridge --subnet=172.16.0.0/16 extnetwork
